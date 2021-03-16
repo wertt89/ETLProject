c5e5d8870d69576c9eb125f123c8ae36c64e42a7
Victoria A <victoria.arriola@northwestern.edu>
2021-03-16 17:01:24 -0500

    messing up query

---
+++ query.sql
@@ -0,0 +1,20 @@
+CREATE TABLE bitcoin_tweet (
+id INT PRIMARY KEY,
+Date TEXT,
+Tweet Count TEXT
+);
+
+
+CREATE TABLE new_bitcoin (
+id INT PRIMARY KEY,
+date TEXT,
+Close TEXT,
+Open TEXT
+);
+
+CREATE TABLE new_ethereum (
+id INT PRIMARY KEY,
+date TEXT,
+Close TEXT,
+Open TEXT
+);
