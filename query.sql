1bf365e066ba0bf4d4b08fdbebee6bd000fa71f8
Judith Samuels <fromjandp@aol.com>
2021-03-16 21:16:22 -0500

    Update Jupyter Note and SQL query

--- query.sql
+++ query.sql
@@ -1,3 +1,11 @@
+
+-- Drop Tables just in case there is a need
+
+-- DROP TABLE new_bitcoin_data_transformed;
+-- DROP TABLE new_ethereum_data_transformed IF Exists;
+
+-- Create Table called:  new_bitcoin_data_transformed 
+
 CREATE TABLE new_bitcoin_data_transformed (
 unix_timestamp VARCHAR (10),
 date date PRIMARY KEY,
@@ -6,6 +14,8 @@ low FLOAT(2),
 close FLOAT(2),
 open FLOAT(2)
 );
+
+-- Create Table called:  new_ethereum_data_transformed 
 
 CREATE TABLE new_ethereum_data_transformed (
 unix_timestamp VARCHAR (10),
@@ -16,5 +26,24 @@ close FLOAT(2),
 open FLOAT(2)
 );
 
-select * from new_bitcoin_data_transformed ;
-select * from new_ethereum_data_transformed ;
+-- verify there is data in each table  --
+
+select * from new_bitcoin_data_transformed LIMIT 200;
+select * from new_ethereum_data_transformed LIMIT 200;
+
+-- Join the two tables
+
+SELECT new_bitcoin_data_transformed.date,
+  new_bitcoin_data_transformed.unix_timestamp,
+  new_bitcoin_data_transformed.high,
+  new_bitcoin_data_transformed.low,
+  new_bitcoin_data_transformed.close,
+  new_bitcoin_data_transformed.open,
+  new_ethereum_data_transformed.unix_timestamp,
+  new_ethereum_data_transformed.high,
+  new_ethereum_data_transformed.low,
+  new_ethereum_data_transformed.close,
+  new_ethereum_data_transformed.open
+FROM new_bitcoin_data_transformed
+JOIN new_ethereum_data_transformed 
+ON new_bitcoin_data_transformed.date = new_ethereum_data_transformed.date;
